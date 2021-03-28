0f80fb7962a2240fe88a0795827a52758f5c3a49
Putri <utyputrijuliani@gmail.com>
2021-03-28 18:27:07 +0700

    first commit

---
+++ PBO6-2011431557-Latihan54-Koordinat/src/pbo6/pkg2011431557/latihan54/koordinat/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6.pkg2011431557.latihan54.koordinat;
+
+/**
+ *
+ * Nama : Putri juliani
+ * Kelas : PBO FS112B
+ * NIM : 2011431557
+ */
+public class Main {
+     public static void main(String[] args) { 
+        warnaKoordinat wk = new warnaKoordinat(10, 4, "Jingga");
+        Koordinat k = new Koordinat(10, 4);
+        System.out.println("Warna Koordinat : " + wk.getNamaWarna());
+        System.out.println("Koordinat Sumbu x : " + k.x + ", " + " y : " + k.y);
+    }
+}
